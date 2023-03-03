4e29a3ecb2781fc0b25fa84fb0d9a8dc3e23a15d
jackie2806 <jaishackeline@gmail.com>
2023-03-02 21:37:09 -0500

    un ejercicio del día 4

--- src/day-4.js
+++ src/day-4.js
@@ -1,3 +1,39 @@
+function getPetExerciseInfo(type, age) {
+    // Tu Código aquí 👈
+    switch (true) {
+      case type === 'perro' && age < 1:
+        return 'Los cachorros necesitan pequeñas y frecuentes sesiones de juego';
+        break;
+      case type === 'perro' && age > 1 && age < 7:
+        return 'Los perros a esta edad necesitan caminatas diarias y sesiones de juego';
+        break;
+      case type === 'perro' && age > 7:
+        return 'Los perros viejos necesitan caminatas más cortas';
+        break;
+      case type === 'gato' && age < 1:
+        return 'Los gatitos necesitan frecuentes sesiones de juego';
+        break;
+      case type === 'gato' && age > 1 && age < 7:
+        return 'Los gatos a esta edad necesitan jugar diariamente';
+        break;
+      case type === 'gato' && age > 7:
+        return 'Los gatos viejos necesitan sesiones de juego más cortas';
+        break;
+      case type === 'ave' && age < 1:
+        return 'Las aves jóvenes necesitan mucho espacio para volar';
+        break;
+      case type === 'ave' && age > 1 && age < 7:
+        return 'Las aves necesitan jugar diariamente y un lugar para volar';
+        break;
+      case type === 'ave' && age > 7:
+        return 'Las aves mayores necesitan descansar más, pero siguen ocupando un lugar para volar';
+        break;
+      default:
+        return 'Tipo de mascota inválida';
+    }
+}
+//console.log(getPetExerciseInfo('perro', 3))
+
 function printTriangle(size, character) {
     //let triangle = '';
    // let line = '';
@@ -9,8 +45,8 @@ function printTriangle(size, character) {
       
     }
       
-    return 
-   
+    
+   return 'hola';
   }
   
-printTriangle(3, '%')
+console.log(printTriangle(3, '%'))
