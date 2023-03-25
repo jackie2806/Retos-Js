60d6c6d661a337e9bdcc69c008c4aa52a6f20919
jackie2806 <jaishackeline@gmail.com>
2023-03-25 13:41:17 -0500

    comentarios a la función que recibe otra función como parámetro

--- src/day-9.js
+++ src/day-9.js
@@ -3,10 +3,25 @@ function add (a,b){
 }
 
 const operation = add; // se guarda la declaración de la función en esta variable
-console.log(operation)
+//console.log(operation)
 
 function makeOperation(operation, num1, num2){
     return operation(num1, num2); //se llama a la función y se le asigna los valores a ejecutar en sus argumentos. 
 }
 
-console.log(makeOperation(operation,2,3))
+console.log(makeOperation(add,2,3))
+
+// una curiosidad descubierta
+
+/* function add (a,b){
+    return a + b;
+}
+
+//const operation = add; // se guarda la declaración de la función en esta variable
+//console.log(operation)
+
+function makeOperation(add, num1, num2){
+    return add(num1, num2); //se llama a la función y se le asigna los valores a ejecutar en sus argumentos. 
+}
+
+console.log(makeOperation(add,2,3)) */
