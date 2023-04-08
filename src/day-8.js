6f11ca390f189afa24b35ea1ca115b6e76eca1dd
jackie2806 <jaishackeline@gmail.com>
2023-04-08 03:00:20 -0500

    Descomentando comentarios a la solución con .push

--- src/day-8.js
+++ src/day-8.js
@@ -78,7 +78,7 @@ console.log(sum1.add(2))
 Recibirás como parámetros un array y una función (func). El array contendrá un conjunto de elementos (números, objetos, strings, etc.) y la función se utilizará para aplicar una acción sobre cada elemento del array. Tu objetivo es devolver un nuevo array con los resultados de la función tal y como lo haría el método map.
  */
 
-/* function myMap(array, func) {
+function myMap(array, func) {
   const newArr = [];
  
   for (let i = 0; i <= array.length - 1; i++){
@@ -89,7 +89,7 @@ Recibirás como parámetros un array y una función (func). El array contendrá 
   return newArr;
   
   
-} */
+}
 // Sin usar push
 
 
