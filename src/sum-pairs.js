590534f82273596ad5b40adf57c50f8dd16e248e
jackie2806 <jaishackeline@gmail.com>
2023-04-25 12:23:18 -0500

    comentarios de los ciclos

--- src/sum-pairs.js
+++ src/sum-pairs.js
@@ -1,11 +1,13 @@
 const n = 6;
 const k = 3;
-const arr = [1, 3, 2, 6, 1, 2]
+const ar = [1, 3, 2, 6, 1, 2]
 function divisibleSumPairs(n, k, ar) {
     // Write your code here
     let count = 0;
   for (let i = 0; i < n; i++) {
+   // console.log('soy i',ar[i])
     for (let j = i+1; j < n; j++) {
+        //console.log('soy j', ar[j])
       if ((ar[i] + ar[j]) % k === 0) {
         count++;
       }
@@ -14,3 +16,5 @@ function divisibleSumPairs(n, k, ar) {
   return count;
 
 }
+
+console.log(divisibleSumPairs(n, k, ar))
