12ce0a316e9da27103866bb3c6ceb8aa99f43b8d
jackie2806 <jaishackeline@gmail.com>
2023-05-05 12:53:48 -0500

    fibonacci solución 1

--- src/recursividad.js
+++ src/recursividad.js
@@ -1,6 +1,6 @@
 // Escribe una función que calcule la suma de los primeros n números enteros de forma recursiva
 
-function sum (n){
+function sum(n){
     if(n === 0){
         return 0;
     } else {
@@ -10,3 +10,22 @@ function sum (n){
 }
 
 console.log(sum(3));
+
+//Escribe una función que calcule la sucesión de Fibonacci de forma recursiva.
+//Fibonacci: serie de números que empieza por 0 y 1, y cada número es la suma de las dos anteriores
+
+//solución usando la posición
+function fibonacci(n){
+   
+    if (n < 2) {
+        return n;
+      } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+      }
+      
+    
+}
+
+console.log(fibonacci(6))
+
+//solución usando el valor
