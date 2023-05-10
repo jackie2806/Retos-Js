4315052bdd0f9d8526bb8a5450f6eb1efaacc338
jackie2806 <jaishackeline@gmail.com>
2023-05-10 15:39:01 -0500

    Comentando la solución

--- src/factor-multiplicador.js
+++ src/factor-multiplicador.js
@@ -1,23 +1,26 @@
 function solution(numbers) {
-    // Tu código aquí 👈  
+    
+    //empezamos con el primer factor 
     let factor1 = 3;  
-    console.log('aquí', factor1)
+  
+    const arrResult = []
     const lengthNumbers = numbers.length;
     for (let i = 0; i < lengthNumbers; i++) {
-        //let factor1 = 3;
-        console.log('i',numbers[i])
+      
       const factor2 = Math.round(numbers[i] / factor1);
-      console.log('factor2', factor2)
-      if (factor1 * factor2 === numbers[i]) {        
-       //factor1 += 1;        
-        console.log('factor1', factor1)
-        return factor2;
-      } else {
-        return false;
-      }
+        
+        arrResult.push(factor2)
+        //aumentamos el factor hasta el número de iteraciones del ciclo
+        factor1 += 1;
     }
-    factor1 += 1;
+    
+    console.log(arrResult)
+    return arrResult
+     
   }
+
+
+  
   //solution([27, 36, 45, 54, 63, 72])
   //console.log()
   solution([27, 36, 45, 54, 42, 72])
