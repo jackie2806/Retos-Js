438a9a3c90fa697a1eb504176ab1f7d9b7b9b31a
jackie2806 <jaishackeline@gmail.com>
2023-05-10 16:10:02 -0500

    Solución testeada

--- src/factor-multiplicador.js
+++ src/factor-multiplicador.js
@@ -1,41 +1,36 @@
 function solution(numbers) {
-    
-    //empezamos con el primer factor 
-    let factor1 = 3;  
-    let factor2 = 1;
-    const arrResult = []
-    const lengthNumbers = numbers.length;
-    for (let i = 0; i < lengthNumbers; i++) {
-      
-      factor2 = Math.round(numbers[i] / factor1);
-        
-        arrResult.push(factor2)
-        //aumentamos el factor hasta el número de iteraciones del ciclo
-        factor1 += 1;
-    }
-    const lengthArrResult = arrResult.length;
-    let multiplier;
-    for(let i = 0; i < lengthArrResult; i++){
-      for(let j = i + 1; j < lengthArrResult; j++){
-        if(arrResult[i] === arrResult[j]){
-          multiplier = true;
-        } else{
-          multiplier = false;
-        }
+  //empezamos con el primer factor
+  let factor1 = 3;
+  let factor2 = 1;
+  const arrResult = [];
+  const lengthNumbers = numbers.length;
+  for (let i = 0; i < lengthNumbers; i++) {
+    factor2 = Math.round(numbers[i] / factor1);
+
+    arrResult.push(factor2);
+    //aumentamos el factor hasta el número de iteraciones del ciclo
+    factor1 += 1;
+  }
+  const lengthArrResult = arrResult.length;
+  let multiplier;
+  for (let i = 0; i < lengthArrResult; i++) {
+    for (let j = i + 1; j < lengthArrResult; j++) {
+      if (arrResult[i] === arrResult[j]) {
+        multiplier = true;
+      } else {
+        multiplier = false;
       }
     }
-    //console.log(factor2)
-    if(multiplier){
-      multiplier = factor2;
-    
-    } 
-    console.log(multiplier)
-    return multiplier;
-     
   }
+  //console.log(factor2)
+  if (multiplier) {
+    multiplier = factor2;
+  }
+  console.log(multiplier);
+  return multiplier;
+} 
 
-
-  solution([6, 8, 10, 12, 14, 16]);
-  solution([27, 36, 45, 54, 63, 72]);
-  //console.log()
-  solution([27, 36, 45, 54, 42, 72]);
+solution([6, 8, 10, 12, 14, 16]);
+solution([27, 36, 45, 54, 63, 72]);
+//console.log()
+solution([27, 36, 45, 54, 42, 72]);
