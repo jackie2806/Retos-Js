e26531d13255b064c1fa4fe53c2d158db97fabd8
jackie2806 <jaishackeline@gmail.com>
2023-05-10 15:13:32 -0500

    solución parcial

---
+++ src/factor-multiplicador.js
@@ -0,0 +1,22 @@
+function solution(numbers) {
+    // Tu código aquí 👈  
+    let factor1 = 3;  
+    console.log('aquí', factor1)
+    const lengthNumbers = numbers.length;
+    for (let i = 0; i < lengthNumbers; i++) {
+        //let factor1 = 3;
+        console.log('i',numbers[i])
+      const factor2 = Math.round(numbers[i] / factor1);
+      console.log('factor2', factor2)
+      if (factor1 * factor2 === numbers[i]) {        
+       //factor1 += 1;        
+        console.log('factor1', factor1)
+        return factor2;
+      } else {
+        return false;
+      }
+    }
+    factor1 += 1;
+  }
+  solution([27, 36, 45, 54, 63, 72])
+  //console.log()
